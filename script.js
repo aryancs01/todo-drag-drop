845e298fd37f8536426a9f908179854a0a289052
Aryan Saxena <aryansaxenacs@gmail.com>
2024-09-15 19:53:22 +0530

    js file modified

--- script.js
+++ script.js
@@ -1,70 +1,83 @@
 let ctr = 0; 
 
-function addTodo(button){
+function addTodo(button) {
     const input = document.createElement("input");
     input.type = "text";
-    input.id = "input-"+ctr;
+    input.id = "input-" + ctr;
 
     const save = document.createElement("button");
-    save.innerHTML = "Save ToDo"
-    save.onclick = function(){
-        saveTodo(ctr,button);
-        ctr+=1
-    }
+    save.innerHTML = "Save ToDo";
+    save.onclick = function () {
+        saveTodo(ctr, button);
+        ctr += 1;
+    };
 
     const div = document.createElement("div");
     div.append(input);
     div.append(save);
 
-
-    button.parentNode.querySelector(".todo-list").append(div)
+    button.parentNode.querySelector(".todo-list").append(div);
     button.style.display = "none";
 }
 
-function saveTodo(num,button){
-    const inputEle = document.getElementById("input-"+num);
+function saveTodo(num, button) {
+    const inputEle = document.getElementById("input-" + num);
     const value = inputEle.value;
-    
+
+    const deleteBtn = document.createElement("button");
+    deleteBtn.innerHTML = "Delete";
+    deleteBtn.setAttribute("onclick", "deleteTodo(this)");
+
     const h4 = document.createElement("h4");
     h4.innerText = value;
-    h4.className = "todo-heading"
-    h4.setAttribute("draggable",true);
-    h4.id = "todo-item-"+num;
-   
-    const parentDiv =  inputEle.parentNode;
-    parentDiv.replaceChildren(h4)
+    h4.className = "todo-heading";
 
-    button.style.display = "block"
+    // Create a container for both h4 and the delete button
+    const todoContainer = document.createElement("div");
+    todoContainer.className = "todo-container";
+    todoContainer.id = "todo-item-" + num;
+    todoContainer.setAttribute("draggable", true);
+
+    todoContainer.appendChild(h4);
+    todoContainer.appendChild(deleteBtn);
+
+    const parentDiv = inputEle.parentNode;
+    parentDiv.replaceChildren(todoContainer);
+
+    button.style.display = "block";
 
     setupDragAndDrop();
-    
+}
+
+function deleteTodo(button) {
+    button.parentNode.remove();
 }
 
 function setupDragAndDrop() {
-    const lists =  document.querySelectorAll(".todo-list");
+    const lists = document.querySelectorAll(".todo-list");
 
-    lists.forEach(list=>{
-        list.addEventListener("dragover", function(e){
+    lists.forEach((list) => {
+        list.addEventListener("dragover", function (e) {
             e.preventDefault();
-        })
+        });
 
-        list.addEventListener("drop",function(e){
-            const selected =  document.querySelector(".dragging");
-            if(selected){
+        list.addEventListener("drop", function (e) {
+            const selected = document.querySelector(".dragging");
+            if (selected) {
                 list.appendChild(selected);
             }
-        })
-    })
+        });
+    });
 
-    const todoItems = document.querySelectorAll(".todo-heading");
+    const todoItems = document.querySelectorAll(".todo-container");
 
-    todoItems.forEach((item)=>{
-        item.addEventListener("dragstart",function(){
-            item.classList.add("dragging")
-        })
+    todoItems.forEach((item) => {
+        item.addEventListener("dragstart", function () {
+            item.classList.add("dragging");
+        });
 
-        item.addEventListener("dragend",function(){
-            item.classList.remove("dragging")
-        })
-    })
+        item.addEventListener("dragend", function () {
+            item.classList.remove("dragging");
+        });
+    });
 }
